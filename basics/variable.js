3e9c9cbef1be0036ce2d34b3bf1105929f09968e
abhijeetpandey0008 <156208339+abhijeetpandey0008@users.noreply.github.com>
2024-04-15 16:44:36 +0000

    all the theoritical partsb are  here

---
+++ basics/variable.js
@@ -0,0 +1,77 @@
+// const  accouuntId = "12344"
+// let accouuntEmail = "abcderfrf1212@gmail.com"
+
+// var accouuntPassword = "1234"
+
+// accountCity  = "jaipur"
+
+
+
+ //console.table([]) // we can use this to display the data in the form of table , and we can pass  multiple values which we want to display the data 
+
+ //console.table([ accouuntId,  accouuntEmail , accouuntPassword])
+
+/*
+ prefer not to use "var "  becouse of issue in block scope and functional scope 
+*/
+
+
+// DATA TYPES IN JS 
+
+//alert(3 + 3 ) // this will not popup the alert slider like as when we code it on broweser  becouse we are using NODE JS  this time 
+
+// number 
+// String
+// Boolean
+// null => standalone value
+// undefined
+// Symbol => uniquness
+
+
+// let score = "33abs"
+
+// console.log(typeof score )
+// console.log(typeof(score))
+
+// let valurInNumber = Number(score)
+// console.log(valurInNumber)
+
+// console.log(typeof valurInNumber)
+
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn  = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+// "" = false in boolean conversion 
+// "any charrecter"  = return true in boolean conversion 
+
+// *******           OPERATION  IN JAVA SCRIPT       //
+
+ // Arithmetic
+ // logical
+
+// Some conparisons 
+
+// console.log(null > 0) ;
+// console.log(null == 0) ; 
+// console.log(null >= 0)
+
+// console.log(undefined == 0);
+
+// comparison and the equality checks both work differently in java script
+
+/// === it strickly check for both the number and their data type also 
+
+
+
+// DATA TYPES
+//  1. PRIMITIVE DATA TYPE 
+// 7 types =>  String , Number , Boolean , null , undefine, symbol , BigInt
+// 2. Reference (Non-primitive) 
+
+// Array , objects , Functions 
+
+
+ 
