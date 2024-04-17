51dd3011bf176bad03fe9223bff79b36693b2bd4
abhijeetpandey0008 <156208339+abhijeetpandey0008@users.noreply.github.com>
2024-04-17 12:49:01 +0000

    know about the numbers and maths function inside the java script

---
+++ basics/nums_math.js
@@ -0,0 +1,10 @@
+// ********************NUMS*********//
+
+const score = 400
+console.log(score);
+
+const balance = new Number(200)
+console.log(balance)
+console.log(balance.toString().length);
+console.log(balance.toFixed(2))
+
