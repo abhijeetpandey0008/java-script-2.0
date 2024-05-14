bdc3def8a3d215df7ce4875fd9e1053327a4b89f
abhijeetpandey0008 <156208339+abhijeetpandey0008@users.noreply.github.com>
2024-05-14 05:25:24 +0000

    functions

---
+++ basics/function.js
@@ -0,0 +1,89 @@
+// // // // **********FUNCTIONS *********
+// // // //  without parenthesis if we call function it will  be like the reference only 
+// // // // function addTwoNumbers(number1, number2){// this is parameter
+// // // //     console.log(number1 + number2);
+// // // // }
+// // // function addTwoNumbers(number1, number2){// this is parameter
+// // //     // let result = number1 + number2
+
+// // //     // return result // after return nothing will be printed 
+// // //     // console.log("Abhijeet ") // this will not be printed 
+// // //     return number1 + number2
+// // // }
+
+// // // const result = addTwoNumbers(5 , 4)// argumnet is passed in this  // calling of function we will pass value in the parenthesis 
+
+// // // // console.log("Result : "  , result) ; 
+
+// // // function loginUserMessage(username){
+// // //     return `${username} just logged in `
+
+// // // }
+
+// // // loginUserMessage("Abhijeet Pandey ") // nothing will be printed becouse we are not saying it to print we just passing the argument 
+// // // console.log(loginUserMessage("Abhijeet Pandey ") ) // this will print 
+// // // console.log(loginUserMessage(" ") )
+// // // console.log(loginUserMessage() )
+
+
+// // // function calculateCardPrice(...num1){ // ... - it is the rest operator used to pass all the value 
+// // //     // rest operator convert the argument in the form of array 
+
+// // //     return num1
+
+// // // }
+
+// // // console.log(calculateCardPrice(200,300,4000))
+
+// // const user  ={
+// //     username:"abhijeeet " , 
+// //     price : 500
+// // }
+
+// // function handleObject(anyobject){
+// //     console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+
+// // }
+// // handleObject(user)
+// // handleObject({
+// //     username : "nikki",
+// //     price : 5000
+
+// // })
+
+
+
+// // ********   SCOPE *************
+
+// // {} -  this is scope 
+
+// // we have 2 scopes 
+// // 1 - Block scope   , 2 - Global scope 
+
+// let a = 300
+// if(true){
+//     let a = 10
+
+//     const b = 20        // all the thing written in if() is called the block code rest are called as global scope 
+
+//     //var c = 30
+//     //console.log("INNER : "  , a)
+// }
+
+
+// console.log(b)
+// console.log(a)
+
+
+function one(){
+    const username = "abhijeet "
+    function two(){
+        const website = "cnjsjheekeke"
+        console.log(username);
+    } // here two can access  all the variable  which is present in one 
+    //console.log(website);
+    two()
+}
+one()
+
+
